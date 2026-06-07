29bf6c00ab2bd5d5a0e58c1b644e2e3139b0a739
vasilanastasov <vasilanastasov_zh24@schoolmath.eu>
2026-06-07 21:24:42 +0300

    final product

---
+++ 03_Работни_файлове/new website/mapdata.js
@@ -0,0 +1,264 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#ffffff",
+    background_transparent: "no",
+    border_color: "#ffffff",
+    popups: "detect",
+        
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#a1afbb",
+    state_hover_color: "#1f425f",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "yes",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AL: {
+      name: "Albania"
+    },
+    AM: {
+      name: "Armenia"
+    },
+    AT: {
+      name: "Austria",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina"
+    },
+    BE: {
+      name: "Belgium",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    BG: {
+      name: "Bulgaria",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    CH: {
+      name: "Switzerland"
+    },
+    CY: {
+      name: "Cyprus",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    CZ: {
+      name: "Czech Republic",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    DE: {
+      name: "Germany",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    DK: {
+      name: "Denmark",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    EE: {
+      name: "Estonia",
+      color: "#3b729f",
+      inactive: "no"
+    },
+    ES: {
+      name: "Spain",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    FI: {
+      name: "Finland",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    FR: {
+      name: "France",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    GB: {
+      name: "United Kingdom"
+    },
+    GE: {
+      name: "Georgia"
+    },
+    GR: {
+      name: "Greece",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    HR: {
+      name: "Croatia",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    HU: {
+      name: "Hungary",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    IE: {
+      name: "Ireland",
+      color: "#3b729f",
+      inactive: "no"
+    },
+    IS: {
+      name: "Iceland"
+    },
+    IT: {
+      name: "Italy",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    LT: {
+      name: "Lithuania",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    LU: {
+      name: "Luxembourg",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    LV: {
+      name: "Latvia",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MK: {
+      name: "North Macedonia"
+    },
+    NL: {
+      name: "Netherlands",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    NO: {
+      name: "Norway"
+    },
+    PL: {
+      name: "Poland",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    PT: {
+      name: "Portugal",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    RO: {
+      name: "Romania",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SE: {
+      name: "Sweden",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    SI: {
+      name: "Slovenia",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    SK: {
+      name: "Slovakia",
+      inactive: "no",
+      color: "#3b729f"
+    },
+    TR: {
+      name: "Turkey"
+    },
+    UA: {
+      name: "Ukraine"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "35.9375",
+      lng: "14.3754",
+      name: "Malta",
+      color: "#3b729f",
+      size: "10"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
